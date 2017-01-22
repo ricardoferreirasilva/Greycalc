157f739c65386d9ab3c9727f3bf92b6ea1efdcb5
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2017-01-22 20:44:51 +0000

    Numerical interfacing done.

--- index.android.js
+++ index.android.js
@@ -3,52 +3,97 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+var {s_height, s_width} = Dimensions.get('window');
+var s = require('./styles/styling.js');
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Container, Header, Title, Content, H1,Footer, FooterTab, Icon } from 'native-base';
 import React, { Component } from 'react';
 import {
+  Dimensions,
   AppRegistry,
+  TouchableOpacity ,
+  TouchableHighlight,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-
+class InputCell extends Component
+{
+  _onClick(token)
+  {
+      console.log(token);
+  }
+  render()
+  {
+    return(
+      <TouchableHighlight style={s.middleRow} 
+          underlayColor = "grey"
+          onPress={() => this.props._pushToken(this.props.token)}>
+          <Row style={{justifyContent: 'center'}} >
+              <Text style={s.calculationText}>{this.props.token}</Text>
+          </Row>
+      </TouchableHighlight>
+    )
+  }
+};
+class InputCellBottom extends Component
+{
+  _onClick(token)
+  {
+      console.log(token);
+  }
+  render()
+  {
+    return(
+      <TouchableHighlight style={s.middleRowBottom} 
+          underlayColor = "grey"
+          onPress={() => this.props._pushToken(this.props.token)}>
+          <Row style={{justifyContent: 'center'}} >
+              <Text style={s.calculationText}>{this.props.token}</Text>
+          </Row>
+      </TouchableHighlight>
+    )
+  }
+};
 export default class Greycalc extends Component {
+  _pushToken(token)
+  {
+      console.log("Parent : "+ token);
+  }
   render() {
     return (
-      <Container>
-                <Header>
-                    <Button transparent>
-                        <Icon name='ios-arrow-back' />
-                    </Button>
-                    
-                    <Title>Header</Title>
-                    
-                    <Button transparent>
-                        <Icon name='ios-menu' />
-                    </Button>
-                </Header>
-            </Container>
+      <Container>     
+          <Content contentContainerStyle={{flex: 1}} style={{padding: 0}}>
+            <Grid >
+                  <Row size={20} style={s.topBar}>
+                      <Text style={s.calculationText}> 5 + 5 = 10 </Text>
+                  </Row>
+                  <Row size={55} style={s.middleBar}>
+                      <Col style={s.middleColumn}>
+                           <InputCell token={1} _pushToken = {this._pushToken} ></InputCell>         
+                           <InputCell token={4} _pushToken = {this._pushToken} ></InputCell>
+                           <InputCell token={7} _pushToken = {this._pushToken} ></InputCell>
+                           <InputCellBottom token={""} _pushToken = {this._pushToken} ></InputCellBottom>
+                      </Col>
+                      <Col style={s.middleColumn}>
+                            <InputCell token={2} _pushToken = {this._pushToken} ></InputCell>         
+                            <InputCell token={5} _pushToken = {this._pushToken} ></InputCell>
+                            <InputCell token={8} _pushToken = {this._pushToken} ></InputCell>
+                            <InputCellBottom token={0} _pushToken = {this._pushToken} ></InputCellBottom>
+                      </Col>
+                      <Col style={s.middleColumn}>
+                            <InputCell token={3} _pushToken = {this._pushToken} ></InputCell>         
+                            <InputCell token={6} _pushToken = {this._pushToken} ></InputCell>
+                            <InputCell token={9} _pushToken = {this._pushToken} ></InputCell>
+                            <InputCellBottom token={"="} _pushToken = {this._pushToken} ></InputCellBottom>                  
+                      </Col>
+                  </Row>
+                  <Row size={12.5} style={{backgroundColor : "#757575"}}></Row>
+                  <Row size={12.5} style={{backgroundColor : "#424242"}}></Row>
+            </Grid>
+          </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('Greycalc', () => Greycalc);
---
+++ styles/styling.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var React = require('react-native');
+
+var myStyles = React.StyleSheet.create({
+   topBar: {
+       backgroundColor : "#424242",
+       /*justifyContent: 'center'*/
+    },
+   calculationText: {
+       color: "white",
+       fontSize: 40,
+       alignSelf: 'center',
+   },
+   middleBar: {
+       backgroundColor : "#616161",
+       padding: 2
+    },
+    middleColumn: {
+        backgroundColor : "#757575",
+        borderColor : "#616161",
+        borderWidth: 2,
+        
+
+    },
+    middleRow: {
+        backgroundColor : "#757575",
+        borderBottomColor : "#616161",
+        borderBottomWidth: 4,
+        flex: 1,
+        justifyContent: 'center',    
+    },
+    middleRowBottom: {
+        backgroundColor : "#757575",
+        flex: 1,
+        justifyContent: 'center',    
+    },
+    numericButton: {
+        alignSelf: 'center',
+        
+        
+    },
+    bottomBar: { }
+})
+module.exports = myStyles;
