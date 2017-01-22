c99399d7d58852a121cc1e5c6cf51cb8807c838a
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2017-01-22 15:31:57 +0000

    First commit.

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class Greycalc extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          First commit.
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
